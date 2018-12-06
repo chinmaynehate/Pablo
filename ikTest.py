3f5ead2940766051bc65339b33608a7928323f99
Chinmay <=>
2018-12-06 14:16:04 +0530

    UART Module to Test for Proper Angles

--- ikTest.py
+++ ikTest.py
@@ -1,20 +1,25 @@
 import pablo as arm
 
 import time as e
-if __name__=="__main__":
-    t=0
-    while(1):
 
-        while(t<4):
-            arm.goToXY(2,t,-8,0)
-            t+=0.5
-            print(t)
-            e.sleep(2)
-        while(t>0):
-            arm.goToXY(2,t,-8,0)
-            t-=0.5
-            print(t)
-            e.sleep(2)
+user = input("enter a ")
+if user == "a":
         
+    if __name__=="__main__":
+        t=0
+        while(1):
 
+            while(t<4):
+                arm.goToXY(2,t,-8,0)
+                t+=0.5
+                print(t)
+                e.sleep(2)
+            while(t>0):
+                arm.goToXY(2,t,-8,0)
+                t-=0.5
+                print(t)
+                e.sleep(2)
+            
+else:
+    print("")
 
