c091dd822856f0ae651071d8ed795c75f13cee46
Chinmay <=>
2018-12-07 14:51:20 +0530

    megapindefs.h added

---
+++ megaPinDefs.h
@@ -0,0 +1,82 @@
+//Atmega128
+
+// A PORT
+
+#define A0 40 
+#define A1 43
+#define A2 42
+#define A3 41
+#define A4 40
+#define A5 39
+#define A6 38
+#define A7 37
+
+//B PORT
+
+#define B0 8
+#define B1 9
+#define B2 10
+#define B3 11
+#define B4 12
+#define B5 13
+#define B6 14
+#define B7 15
+
+ //C PORT
+
+#define C0 28
+#define C1 29
+#define C2 30
+#define C3 31
+#define C4 32
+#define C5 33
+#define C6 34
+#define C7 35
+
+
+//D PORT
+
+#define D0 18
+#define D1 19
+#define D2 20
+#define D3 21
+#define D4 22
+#define D5 23
+#define D6 24
+#define D7 25
+
+
+//E PORT
+
+#define E0 0
+#define E1 1
+#define E2 2
+#define E3 3
+#define E4 4
+#define E5 5
+#define E6 6
+#define E7 7
+
+
+//F PORT
+
+#define F0 45 
+#define F1 46
+#define F2 47
+#define F3 48
+#define F4 49
+#define F5 50
+#define F6 51
+#define F7 52
+
+
+//G PORT
+
+#define G0 26
+#define G1 27
+#define G2 36
+#define G3 16
+#define G4 17
+#define G5 NULL
+#define G6 NULL
+#define G7 NULL
